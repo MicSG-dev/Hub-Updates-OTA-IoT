029c686b6c73338139dfa50ee2cd96b096239366
MicSG <dev@micsg.com.br>
2024-04-09 20:37:00 -0300

    Update recuperar-acesso.php

--- Server-Files/recuperar-acesso.php
+++ Server-Files/recuperar-acesso.php
@@ -1,7 +1,26 @@
 <?php
 
-$nomeArquivoHtml = str_replace('/','', $_SERVER['SCRIPT_NAME']);
+$nomeArquivoHtml = __FILE__;
 $nomeArquivoHtml = str_replace('.php','.html', $nomeArquivoHtml);
-$pageHtml = file_get_contents("./$nomeArquivoHtml");
+$pageHtml = file_get_contents($nomeArquivoHtml);
 
-echo($pageHtml);
+// verificar se usuário ESTA ou não logado. Se estiver logado, redirecionar ele para home. Se NÃO estiver logado, continuar.
+
+isset($_POST["email-recover"]) ? $email_recover = $_POST["email-recover"] : $email_recover = null;
+
+if($email_recover != null){    
+    
+    if(filter_var($email_recover, FILTER_VALIDATE_EMAIL)){
+        // verificar se email esta cadastrado
+        //  se estiver cadastrado, enviar um email de recuperação
+        //  se não, não fazer nada
+        http_response_code(200);
+        echo("OK");
+        
+    }else{
+        http_response_code(400);
+        echo("Email invalido");
+    }
+}else{
+    echo($pageHtml);
+}
