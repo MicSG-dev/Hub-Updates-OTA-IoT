13610a13d020c48fd5d9de62d291f8033e430dd6
MicSG-dev <dev@micsg.com.br>
2024-04-10 18:06:19 -0300

    Atualizada interface de Redefinição de Senha

--- Server-Files/assets/js/gerar-codigo-redefinicao.js
+++ Server-Files/assets/js/gerar-codigo-redefinicao.js
@@ -1,14 +1,36 @@
 window.addEventListener('load', ()=>{
+
+    const carouselElement = document.querySelector('#carousel');
+    carouselElement.classList.remove("slide");
+    
+
+
     this.document.getElementById('gerar-codigo-redefinicao').addEventListener('click', ()=>{
         // fazer request POST na API
         // ...
 
-        const carouselElement = document.querySelector('#carousel');
         const carousel = bootstrap.Carousel.getOrCreateInstance(carouselElement);
         carousel.to(1);
     });
 
-    this.document.getElementById('form-redefinicao').addEventListener('submit', (event)=>{
+    this.document.getElementById('verificar-codigo-redefinicao').addEventListener('click', ()=>{
+        // fazer request POST na API
+        // ...
+
+        const carousel = bootstrap.Carousel.getOrCreateInstance(carouselElement);
+        carousel.to(2);
+    });
+
+    this.document.getElementById('form-gerar-codigo').addEventListener('submit', (event)=>{
         event.preventDefault();
     });
+
+    this.document.getElementById('form-verificar-codigo').addEventListener('submit', (event)=>{
+        event.preventDefault();
+    });
+
+    this.document.getElementById('form-redefinir-senha').addEventListener('submit', (event)=>{
+        event.preventDefault();
+    });
+
 });
