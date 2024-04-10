46a0e2601064c8e1af2c05d44ab479151094c743
MicSG-dev <dev@micsg.com.br>
2024-04-09 21:19:49 -0300

    Atualizado recuperação de senha

--- Server-Files/login.php
+++ Server-Files/login.php
@@ -1,7 +1,7 @@
 <?php
 
-$nomeArquivoHtml = str_replace('/','', $_SERVER['SCRIPT_NAME']);
+$nomeArquivoHtml = __FILE__;
 $nomeArquivoHtml = str_replace('.php','.html', $nomeArquivoHtml);
-$pageHtml = file_get_contents("./$nomeArquivoHtml");
+$pageHtml = file_get_contents($nomeArquivoHtml);
 
 echo($pageHtml);
