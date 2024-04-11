2856b4fb99885ad2946e1a1c6a6a4718c7d2e4ed
MicSG-dev <dev@micsg.com.br>
2024-04-11 00:30:34 -0300

    Adicionar capacidade de exibir texto no display OLED

--- IoT-Code-Example-Use/Device-IoT-1/Example-Use-Hub-Updates-OTA-IoT-Device-1/src/main.cpp
+++ IoT-Code-Example-Use/Device-IoT-1/Example-Use-Hub-Updates-OTA-IoT-Device-1/src/main.cpp
@@ -1,8 +1,31 @@
 #include <Arduino.h>
 
+const int SCREEN_WIDTH = 128;
+const int SCREEN_HEIGHT = 64;
+
+#define SCREEN_ADDRESS 0x3D
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("Hello, world!!!");
+
+  display.display(); 
   
 }
 
