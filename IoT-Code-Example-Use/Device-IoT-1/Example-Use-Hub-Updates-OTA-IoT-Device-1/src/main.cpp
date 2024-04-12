cd8010409139636952db0e02277f89d0888f8526
MicSG-dev <dev@micsg.com.br>
2024-04-12 01:32:06 -0300

    Atualizar prevenção de Burn-in no display OLED do Device1

--- IoT-Code-Example-Use/Device-IoT-1/Example-Use-Hub-Updates-OTA-IoT-Device-1/src/main.cpp
+++ IoT-Code-Example-Use/Device-IoT-1/Example-Use-Hub-Updates-OTA-IoT-Device-1/src/main.cpp
@@ -11,7 +11,6 @@ const int SCREEN_ADDRESS = 0x3C;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-
 void setup()
 {
   Serial.begin(115200);
@@ -19,7 +18,6 @@ void setup()
   Wire.setSDA(12);
   Wire.setSCL(13);
   Wire.begin();
-  
 
   Serial.println("Iniciado Firmware");
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
@@ -28,25 +26,29 @@ void setup()
     for (;;)
       ; // Don't proceed, loop forever
   }
-  else
-  {
-    Serial.println("Iniciado display");
-  }
-
-  display.display();
-
-  delay(1000); // Pause for 2 seconds
-
-  // Clear the buffer
+  delay(1000);
   display.clearDisplay();
-  display.setCursor(15,16);
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
-  display.print("0.1");
+
+  // Diminuição de Contraste e Brilho na tela para prevenção de efeito Burn-In (por ser tela OLED)
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
+  display.ssd1306_command(SSD1306_SETVCOMDETECT);
+  display.ssd1306_command(0x00);
   display.display();
 }
 
 void loop()
 {
-  delay(10);
+  static int i = 0;
+  display.clearDisplay();
+  display.setCursor(15, 16);
+  display.setTextSize(5);
+  display.setTextColor(SSD1306_WHITE);
+  display.print("0.");
+  display.print(i);
+  display.display();
+  delay(1000);
+  i++;
+  if (i >= 10)
+    i = 0;
 }
