25e4cac8f13415c692c56669f73a8f7c5baf8d78
MicSG-dev <dev@micsg.com.br>
2024-04-18 00:13:18 -0300

    Adicionar funcionalidade de solicitar acesso

---
+++ Server-Files/solicitar-acesso.php
@@ -0,0 +1,16 @@
+<?php
+define('database-acesso-privado-rv$he', TRUE);
+
+require('./private/database.php');
+require('./private/credentials.php');
+
+$nomeArquivoHtml = basename(__FILE__);
+$prePath = str_replace($nomeArquivoHtml, "", __FILE__);
+$interPath = "private\\html\\";
+
+$fullPath = str_replace('.php', '.html', $prePath . $interPath . $nomeArquivoHtml);
+$pageHtml = file_get_contents($fullPath);
+
+executarFuncoesDeTodasPaginas($host, $username, $password);
+
+echo ($pageHtml);
