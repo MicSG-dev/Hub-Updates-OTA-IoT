94f1e0559908820baa124e5156880bd10af9acbe
MicSG-dev <dev@micsg.com.br>
2024-04-24 03:16:12 -0300

    Corrigir comentário em credentials.php

--- Server-Files/private/credentials.php
+++ Server-Files/private/credentials.php
@@ -8,6 +8,6 @@ if (!defined('database-acesso-privado-rv$he')) {
     $host = "localhost"; // o host do banco de dados
     $username = "micsg-tests"; // o username do banco de dados
     $password = "micsg-tests"; // a senha do banco de dados
-    $database = "hub_updates_ota_iot"; // o nome da database no banco de dados (NÃO é necessário alterar)
+    $database = "hub_updates_ota_iot"; // (NÃO é necessário alterar) este é o nome da database no banco de dados 
     $chaveJwt = "micsg-tests"; // a chave secreta utilizada para assinar os tokens JWT (altere para qualquer termo, mas que seja SEGURO)
 }
