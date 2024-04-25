8c4c2b7aadce2e8a045b8594cc363814e8ff534e
MicSG-dev <dev@micsg.com.br>
2024-04-25 02:23:36 -0300

    Atualizar arquivos
    Corrigir erro em geração tabela de novo cadastro

---
+++ Server-Files/home.php
@@ -0,0 +1,33 @@
+<?php
+define('database-acesso-privado-rv$he', TRUE);
+
+require('./private/database.php');
+require('./private/credentials.php');
+require('./private/vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$nomeArquivoHtml = basename(__FILE__);
+$prePath = str_replace($nomeArquivoHtml, "", __FILE__);
+$interPath = "private\\html\\";
+
+$fullPath = str_replace('.php', '.html', $prePath . $interPath . $nomeArquivoHtml);
+$pageHtml = file_get_contents($fullPath);
+
+executarFuncoesDeTodasPaginas($host, $username, $password, $database);
+
+$token = isset($_COOKIE["key"]) ? $_COOKIE["key"] : null;
+
+if (estaLogado($token, $chaveJwt) && !estaNaTokenBlackList($host, $username, $password, $database, $token)) {
+    $infoJwt = getInfoTokenJwt($token, $chaveJwt);
+    if ($infoJwt["sub"] == "demo") {
+        header("Location: /novo-cadastro");
+        die();
+    } else {
+        echo ($pageHtml);
+    }
+} else {
+    header("Location: /login");
+    die();
+}
--- Server-Files/private/credentials.php
+++ Server-Files/private/credentials.php
@@ -5,10 +5,16 @@ if (!defined('database-acesso-privado-rv$he')) {
     die("Acesso direto ao \"private/credentials\" não permitido.");
 } else {
 
+    // OBRIGATÓRIO - ALTERE OS VALORES DAS SEGUINTES VARIÁVEIS
     $host = "localhost"; // o host do banco de dados
     $username = "micsg-tests"; // o username do banco de dados
     $password = "micsg-tests"; // a senha do banco de dados
     $pepperHash = "micsg-tests"; // o pepper para geração de hash das senhas dos users (altere para qualquer termo, mas que seja SEGURO)
     $chaveJwt = "micsg-tests"; // a chave secreta utilizada para assinar os tokens JWT (altere para qualquer termo, mas que seja SEGURO)
+    
+    // OPCIONAL - ALTERE OS VALORES DAS SEGUINTES VARIÁVEIS
+    $versaoSistema = "1.0"; // a versão do sistema (possibilita o ADMIN do servidor apagar as tabelas do banco de dados, ou quando fazer alguma alteração significativa, e, quando incrementar a versão do sistema, os usuários que estejam logados anteriormente estarão 'deslogados')
+
+    // NÃO ALTERAR
     $database = "hub_updates_ota_iot"; // (NÃO é necessário alterar) este é o nome da database no banco de dados 
 }
