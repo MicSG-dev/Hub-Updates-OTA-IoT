711389888dd703e9a71bd9b4a564664b8c06c2a8
MicSG-dev <dev@micsg.com.br>
2024-05-02 20:18:54 -0300

    Adicionar end-point gerarr-codigo api web

--- Server-Files/api/web/recuperar-acesso/gerar-codigo.php
+++ Server-Files/api/web/recuperar-acesso/gerar-codigo.php
@@ -1,1 +1,21 @@
 <?php
+
+define('database-acesso-privado-rv$he', TRUE);
+$profundidadePastaAtual = 3;
+$pastaInicial = implode("/", array_slice(explode("\\", __DIR__), 0, -$profundidadePastaAtual));
+
+include_once ($pastaInicial . '/private/database.php');
+include_once ($pastaInicial . '/private/credentials.php');
+include_once ($pastaInicial . '/private/vendor/autoload.php');
+include_once ($pastaInicial . '/private/utils/general.php');
+
+executarFuncoesDeTodasPaginas($host, $username, $password, $database, $emailDemoAccount, $senhaDemoAccount, $chaveCrypto);
+
+// Parâmetro POST email-recover
+$email_recover = isset($_POST["email-recover"]) ?
+    $_POST["email-recover"] :
+    null;
+
+if ($email_recover != null) {
+
+}
